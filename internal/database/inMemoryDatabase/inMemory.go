4d7b240e93e312750397c67cd7576e3e12917c14
wpted <silverhandsome@gmail.com>
2024-02-07 14:13:08 +0800

    Feat: Create method LRange().

--- internal/database/inMemoryDatabase/inMemory.go
+++ internal/database/inMemoryDatabase/inMemory.go
@@ -132,13 +132,28 @@ func (d *Db) Decrement(key string) error {
     return nil
 }
 
-func (d *Db) LRange(key string, left, right int) ([]string, error) {
-    return nil, nil
+// LRange returns the specified elements of the list stored at key.
+// The offsets start and stop are zero-based indexes, with 0 being the first element of the list ( the head of the list ), 1 being the next element and so on.
+// These offsets can also be negative numbers indicating offsets starting at the end of the list.
+// For example, -1 is the last element of the list, -2 the penultimate, and so on.
+// Out of range indexes will not produce an error, if start is larger than the end of the list, an empty array is returned.
+// If stop is larger than the actual end of the list, LRange will treat it like the last element of the list.
+// If the key doesn't exist, returns an empty list.
+func (d *Db) LRange(key string, start, stop int) ([]string, error) {
+    if _, ok := d.stringStorage[key]; ok {
+        // Values having wrong type.
+        return nil, ErrNotList
+    } else if _, ok := d.listStorage[key]; !ok {
+        // Keys that doesn't exist.
+        return nil, nil
+    } else {
+        return d.listStorage[key].LRange(start, stop), nil
+    }
 }
 
 // LeftPush prepends one or more elements to the list.
 // The return value is the Len of the list after the push operation.
-// If key doesn't exists, it is created as empty list before performing the push operations.
+// If key doesn't exist, it is created as empty list before performing the push operations.
 // When key holds a value that is not a list, an error is returned.
 // It is possible to push multiple elements using a single command call just specifying multiple arguments at the end of the command.
 // Elements are inserted one after the other to the head of the list, from the leftmost element to the rightmost element.
