d9226c60f7bab7c56df689dc7d54ca80c5cdeac7
wpted <silverhandsome@gmail.com>
2024-02-12 21:56:53 +0800

    Feat: Create entry point for my Redis server.

--- cmd/main.go
+++ cmd/main.go
@@ -1,60 +1,17 @@
 package main
 
 import (
-    "MyOwnRedis/internal/redisObject"
-    "fmt"
-    "log"
-    "net"
+    "MyOwnRedis/internal/database/inMemoryDatabase"
+    "MyOwnRedis/internal/server"
 )
 
-const RedisDefaultPort = 6378
+const RedisDefaultPort string = "6379"
 
 func main() {
-    // Create a tcp server that listens to port 6379 through TCP.
-    listener, err := net.Listen("tcp", fmt.Sprintf(":%d", RedisDefaultPort))
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("My Redis server listening on port 6379...")
+    db := inMemoryDatabase.New()
+    s := server.New("localhost", RedisDefaultPort, db)
+    defer s.Close()
 
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            log.Println(err)
-            continue
-        }
-
-        // This allows multiple client connections.
-        go func() {
-            defer func(conn net.Conn) {
-                _ = conn.Close()
-            }(conn)
-
-            for {
-                buf := make([]byte, 1024)
-                n, err := conn.Read(buf)
-                if err != nil {
-                    // Send error message back to the client.
-                    // We don't close the connection.
-                    break
-                }
-
-                // Print the incoming data
-                fmt.Printf("Received: %s\n", buf)
-                _, _ = redisObject.Deserialize(buf)
-
-                // Close the connection on receiving specific commands.
-                if string(buf[:n]) == "quit" {
-                    return
-                } else {
-                    ok := []byte("+PONG\r\n")
-                    _, err = conn.Write([]byte(ok))
-                    if err != nil {
-
-                    }
-                }
-            }
-        }()
-    }
+    s.AcceptRequest()
     return
 }
