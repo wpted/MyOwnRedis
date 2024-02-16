a15cd1ebee7cc2efd469eab5798f62d8b1d5ad01
wpted <silverhandsome@gmail.com>
2024-02-16 17:21:13 +0800

    Feat: Implement methods Run(), New() and Close() for RedisServer().

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,77 +4,93 @@ import (
     "MyOwnRedis/internal/database"
     "MyOwnRedis/internal/redisObject"
     "fmt"
-    "log"
     "net"
 )
 
+const TCP = "tcp"
+
+// 1. Create a RedisServer struct
+// 2. Create methods Run(), Close() and Evaluate().
+
 type RedisServer struct {
     addr string
-    l    *net.Listener
-    db   database.MemDb
+    // Passing `net.Listener` by value is idiomatic and aligns with the general practice in Go of passing interface by value.
+    l  net.Listener
+    db database.MemDb
 }
 
-func New(host, port string, db database.MemDb) *RedisServer {
-    addr := fmt.Sprintf("%s:%s", host, port)
-    listener, err := net.Listen("tcp", addr)
-    if err != nil {
-        log.Fatalf("Error listening: %#v.\n", err)
-    }
-
+// New creates a new RedisServer.
+func New(addr string, db database.MemDb) *RedisServer {
     return &RedisServer{
         addr: addr,
-        l:    &listener,
         db:   db,
     }
 }
 
-func (r *RedisServer) Close() {
-    if err := (*r.l).Close(); err != nil {
-        log.Fatalf("Error closing connection: %#v.\n", err)
+// Run sets up a Redis server that can handle multiple client connections concurrently and respond to client requests asynchronously.
+// Each connection is handled in a separate goroutine, allowing the server to remain responsive to new connections and handle them efficiently.
+func (r *RedisServer) Run() error {
+    var err error
+    // Create a socket that can accept incoming connections.
+    r.l, err = net.Listen(TCP, r.addr)
+    if err != nil {
+        return err
+    }
+
+    // Forever loop ran here is necessary to continuously accept incoming connections.
+    for {
+        var conn net.Conn
+        // Waiting for incoming connections.
+        conn, err = r.l.Accept()
+        if err != nil || conn == nil {
+            // If anything happens when waiting for connections, process towards the next loop.
+            // i.e. Error Accepting or Empty connection ( meaning that there's no incoming connection )
+            continue
+        }
+
+        // If receive a connection, spawn the connection dealing process with a goroutine.
+        go func(conn net.Conn) {
+            // Close the connection after we're done dealing with the connection.
+            defer func() {
+                err := conn.Close()
+                if err != nil {
+                    fmt.Println(err)
+                }
+            }()
+            // Read data from the connection.
+            // For loop here is to enable sequential network reads on the same connection,
+            // and will break
+            for {
+                req := make([]byte, 1024)
+                n, err := conn.Read(req)
+                if err != nil {
+
+                    break
+                }
+
+                // Trim empty bytes.
+                req = req[:n]
+
+                // Handle request.
+                var response []byte
+                response, err = r.HandleRequest(req)
+                if err != nil {
+                    break
+                }
+
+                // Write response to the connection (Responding to client).
+                if _, err = conn.Write(response); err != nil {
+                    break
+                }
+            }
+        }(conn)
     }
 }
 
-func (r *RedisServer) AcceptRequest() {
-    for {
-        conn, err := (*r.l).Accept()
-        if err != nil {
-            log.Fatalf("Error accepting: %#v.\n", err)
-        }
-        // Handle connections with goroutines.
-        go func(conn net.Conn) {
-            // Create a buffer to hold incoming request.
-            reqBuf := make([]byte, 1024)
-            // Read the incoming connection into the buffer.
-            _, err = conn.Read(reqBuf)
-            fmt.Println(string(reqBuf))
-
-            if err != nil {
-                log.Println("Error reading request:", err.Error())
-            }
-
-            // Do something with the request buffer.
-            robj, err := redisObject.Deserialize(reqBuf)
-            // Send response back to client.
-            var resp []byte
-            if err != nil {
-                resp = redisObject.Serialize(redisObject.SimpleErrors, err.Error())
-            } else {
-                resp, err = r.Evaluate(robj)
-                if err != nil {
-                    resp = redisObject.Serialize(redisObject.SimpleErrors, err.Error())
-                } else {
-                    if _, err = conn.Write(resp); err != nil {
-                        panic(err)
-                    }
-                }
-            }
-
-            // Close connection.
-            //if err := conn.Close(); err != nil {
-            //    panic(err)
-            //}
-        }(conn)
-    }
+// Close closes the listener.
+// Any blocked `Accept` operations will be unblocked and return errors.
+func (r *RedisServer) Close() error {
+    return r.l.Close()
 }
 
 func (r *RedisServer) Evaluate(robj *redisObject.RObj) ([]byte, error) {
@@ -98,4 +114,19 @@ func (r *RedisServer) Evaluate(robj *redisObject.RObj) ([]byte, error) {
     case "lrange":
     }
     return resp, nil
+}
+
+func (r *RedisServer) HandleRequest(request []byte) ([]byte, error) {
+    var response []byte
+
+    robj, err := redisObject.Deserialize(request)
+    if err != nil {
+        response = redisObject.Serialize(redisObject.SimpleErrors, err.Error())
+    }
+    response, err = r.Evaluate(robj)
+    if err != nil {
+
+    }
+
+    return response, nil
 }
